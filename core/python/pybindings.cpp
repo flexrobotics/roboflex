fba2891718e07d6019d0c99b3009f8b067d9f0f9
Colin Prepscius <colinprepscius@gmail.com>
2023-05-17 12:50:41 -0400

    added python binding for LastOne

--- core/python/pybindings.cpp
+++ core/python/pybindings.cpp
@@ -282,7 +282,9 @@ PYBIND11_MODULE(roboflex_core_python_ext, m)
         .def(py::init<const std::string &>(),
             "Create a node that just remembers the last message, in a thread-safe way.",
             py::arg("name") = "LastOne")
+        .def_property_readonly("last_one", &LastOne::get_last_one)
     ;
+
 
     // ---------- FRP Utility nodes -----------
 
