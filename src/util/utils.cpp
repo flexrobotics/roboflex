95b4a978718e59a47b6edfde6935671eb359e801
Colin Prepscius <colinprepscius@gmail.com>
2023-11-15 20:08:55 -0500

    update version

--- src/util/utils.cpp
+++ src/util/utils.cpp
@@ -23,7 +23,7 @@ std::string repeated_string(const std::string& input, size_t num)
 
 std::string get_roboflex_core_version() 
 {
-    return std::string("0.1.14");
+    return std::string("0.1.16");
 }
 
 double get_current_time() 
